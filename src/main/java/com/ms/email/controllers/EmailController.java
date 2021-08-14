af3acef54280c11d8f3f17c0e5234f0c34662fa0
isaiassantossilva <isaias661248@gmail.com>
2021-08-14 17:08:55 -0300

    first commit

---
+++ src/main/java/com/ms/email/controllers/EmailController.java
@@ -0,0 +1,29 @@
+package com.ms.email.controllers;
+
+import com.ms.email.dtos.EmailDto;
+import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+public class EmailController {
+
+    @Autowired
+    EmailService emailService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/send-email")
+    public EmailModel sendEmail(@RequestBody @Valid EmailDto emailDto){
+        var email = new EmailModel();
+        BeanUtils.copyProperties(emailDto, email);
+        return emailService.sendEmail(email);
+    }
+}
